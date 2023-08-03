304809811ef41461ca3469264a41fb9b9d1382ef
StatsAI <67183539+StatsAI@users.noreply.github.com>
2023-08-03 17:11:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,11 +56,11 @@ if __name__ == "__main__":
 	url = "https://github.com/StatsAI/streamlit_image_search/releases/download/image_search_assets/faiss_assets.zip"
 	download_and_unzip(url)
 	
-	#image_list = Load_Data().from_folder(['animals'])
-	#loaded_index = faiss.read_index("image_features_vectors.idx")
-	#image_data = pd.read_pickle("image_data_features.pkl")
+	image_list = Load_Data().from_folder(['animals'])
+	loaded_index = faiss.read_index("image_features_vectors.idx")
+	image_data = pd.read_pickle("image_data_features.pkl")
 
-	_load_assets()
+	#_load_assets()
 
 
 if "data" not in st.session_state:
