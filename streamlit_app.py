0319554770defaa2bf3892898388f4db32f56824
StatsAI <67183539+StatsAI@users.noreply.github.com>
2023-08-03 14:57:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -126,7 +126,7 @@ def _search_by_vector(v, n: int):
         return dict(zip(I[0], image_data.iloc[I[0]]['images_paths'].to_list()))
         #return image_paths
 
-
+@st.cache_resource
 def plot_similar_images_new(image_path: str, number_of_images: int = 6):
         """
         Plots a given image and its most similar images according to the indexed image features.
