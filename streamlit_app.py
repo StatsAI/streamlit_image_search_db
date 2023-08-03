92a053e89fe3ed49f6d7e003ea32188e7c8b1c95
StatsAI <67183539+StatsAI@users.noreply.github.com>
2023-08-03 15:39:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,18 +41,29 @@ if __name__ == "__main__":
     url = "https://github.com/StatsAI/streamlit_image_search/releases/download/image_search_assets/faiss_assets.zip"
     download_and_unzip(url)
 
-#@st.cache_resource
-def _load_assets():
-	# Load images from a folder
-	image_list = Load_Data().from_folder(['animals'])
+
+# Load images from a folder
+image_list = Load_Data().from_folder(['animals'])
 	
-	# Load indexed images
-	loaded_index = faiss.read_index("image_features_vectors.idx")
+# Load indexed images
+loaded_index = faiss.read_index("image_features_vectors.idx")
 
-	# Load image features
-	image_data = pd.read_pickle("image_data_features.pkl")
+# Load image features
+image_data = pd.read_pickle("image_data_features.pkl")
 
-_load_assets()
+
+#@st.cache_resource
+# def _load_assets():
+# 	# Load images from a folder
+# 	image_list = Load_Data().from_folder(['animals'])
+	
+# 	# Load indexed images
+# 	loaded_index = faiss.read_index("image_features_vectors.idx")
+
+# 	# Load image features
+# 	image_data = pd.read_pickle("image_data_features.pkl")
+
+# _load_assets()
 ####################################################################################################################################################
 
 st.title('Image Recommendation App')
