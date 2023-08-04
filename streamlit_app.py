9d0a291efefb528e5771c7f200e6a8a3db0d3525
StatsAI <67183539+StatsAI@users.noreply.github.com>
2023-08-04 02:05:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,9 +87,6 @@ st.markdown("""
         <style>
                .block-container {
                     padding-top: 1rem;
-                    padding-bottom: 0rem;
-                    padding-left: 5rem;
-                    padding-right: 5rem;
                 }
         </style>
         """, unsafe_allow_html=True)
