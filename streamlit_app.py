d07531fd3af91ce3353c2c3586025e19359fcfad
StatsAI <67183539+StatsAI@users.noreply.github.com>
2023-08-12 18:43:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -164,8 +164,6 @@ def clear_text():
 
 text_input = st.sidebar.text_input("Input window", key="text", on_change=clear_text)
 
-st.session_state["temp"]
-
 ####################################################################################################################################################
 
 
@@ -176,7 +174,8 @@ def plot_similar_images_new(image_path: str, number_of_images: int = 6):
 	# Encode the text you want to return images of.	
 	if text_input != "":
 		animal_embedding = model.encode(text_input)
-		#text_input = ""
+		st.session_state["temp"]	
+	#text_input = ""
 	else:
 		animal_embedding = model.encode(image_path)	
 
