2df91e2e94d4515a1bb4f7ed2521bdd62fa88f7b
StatsAI <67183539+StatsAI@users.noreply.github.com>
2023-08-12 22:32:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -249,7 +249,7 @@ if st.sidebar.button('Get Similar Images'):
 	st.pyplot(plot_similar_images_new(image_path, number_of_images = 20))
 	#text_input = st.sidebar.text_input("Input window", "", key = "text", on_change=clear_input_box)
 	#st.session_state["text"] = st.sidebar.text_input("Input window", "")
-	st.session_state["text"] = ""
+	#st.session_state["text"] = ""
 	text_input = ""
 	#plot_similar_images_new(image_path = image_list[images_recs], number_of_images = 20)
 	#text_input = st.sidebar.text_input("Text Search: Enter an animal's name using text", '')
