0ab7f4d2cbd66a33edd64a5cac6fb19af6a5a2c6
StatsAI <67183539+StatsAI@users.noreply.github.com>
2024-02-15 17:19:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -260,23 +260,14 @@ def plot_similar_images_new(image_path, text_input, number_of_images: int = 6):
 	results = vector_db(client, animal_embedding)
 	results = results[1:]
 
-	# result_image_type = results[0].payload['type']
-	# result_str = "The animal you selected is a " + result_image_type	
-	# #st.write(result_str)
-
-	# llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-	# input_text = "Summarize in 100 words, the most interesting things about the following animal: " + result_str
-	# response = llm(input_text)
-	# st.write(result_str + ". " + response)
-
 	result_image_type = results[0].payload['type']
-	result_str = "You selected the following animal: " + result_image_type + ". Enter an OpenAI API Key to learn about it!"	
+	result_str = "You selected the following animal: " + result_image_type + ". Enter an OpenAI API Key for a summary!"	
 	
 	if openai_api_key != "":		
 		llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
 		input_text = "Summarize in 100 words, the most interesting things about the following animal: " + result_str
 		response = llm(input_text)
-		st.write(result_str + ". " + response)
+		st.write(You selected the following animal: " + result_image_type + ". " + response)
 	else:
 		st.write(result_str)	
 
