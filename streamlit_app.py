269789332d6df1e781d57e9a7dedc3ed51974626
StatsAI <67183539+StatsAI@users.noreply.github.com>
2025-01-30 02:25:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ import json
 
 from langchain_openai import OpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_ollama.llms import OllamaLLM
+#from langchain_core.prompts import ChatPromptTemplate
+#from langchain_ollama.llms import OllamaLLM
 
 from typing import List
 from sentence_transformers import SentenceTransformer, util
