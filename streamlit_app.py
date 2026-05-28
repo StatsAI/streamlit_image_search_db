0d5419c5f4f5d8d3fcf2f65123069c925bb032ad
StatsAI <67183539+StatsAI@users.noreply.github.com>
2026-05-28 15:38:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -230,7 +230,7 @@ def plot_similar_images_new(image_path, text_input, number_of_images: int = 6):
 		# response = llm(input_text)
 		# st.write(response)
 
-		llm = ChatGoogleGenerativeAI(model="gemini-robotics-er-1.6-preview, temperature=0.7, max_tokens=None, timeout=None, max_retries=2, google_api_key=openai_api_key)
+		llm = ChatGoogleGenerativeAI(model="gemini-robotics-er-1.6-preview", temperature=0.7, max_tokens=None, timeout=None, max_retries=2, google_api_key=openai_api_key)
 		input_text = "Summarize in 100 words, the most interesting things about the following animal: " + result_image_type
 		response = llm.invoke(input_text)
 		st.write(response.content)
